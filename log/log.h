06eedc05767f77c49b0dbaa1d014afd1e1930b67
tkal00 <timo.kaljazin@hotmail.com>
2024-07-16 22:22:33 +0200

    verboser logs

--- log/log.h
+++ log/log.h
@@ -1,6 +1,7 @@
 #ifndef AMBER_LOG
 #define AMBER_LOG
 #include <sstream>
+#include <source_location>
 
 namespace amber::log
 {
@@ -14,16 +15,16 @@ namespace amber::log
                 fatal
         };
         void setLevel(Level level);
-
         void setGlobalSink(std::ostream& sink);
         void write(Level level, std::ostringstream&& buffer);
 }
 
-#define LOG_TRACE(x)    amber::log::write(amber::log::trace,    std::ostringstream() << "[TRC] " << x);
-#define LOG_DEBUG(x)    amber::log::write(amber::log::debug,    std::ostringstream() << "[DBG] " << x);
-#define LOG_INFO(x)     amber::log::write(amber::log::info,     std::ostringstream() << "[INF] " << x);
-#define LOG_WARN(x)     amber::log::write(amber::log::warn,     std::ostringstream() << "[WRN] " << x);
-#define LOG_ERROR(x)    amber::log::write(amber::log::error,    std::ostringstream() << "[ERR] " << x);
-#define LOG_FATAL(x)    amber::log::write(amber::log::fatal,    std::ostringstream() << "[FTL] " << x);
+#define LOG_TRACE(x)    do { const auto loc = std::source_location::current(); amber::log::write(amber::log::trace, std::ostringstream() << "[TRC] " << loc.file_name() << '(' << loc.line() << ',' << loc.column() << ") " << loc.function_name() << ": \t" << x); } while (false)
+#define LOG_DEBUG(x)    do { const auto loc = std::source_location::current(); amber::log::write(amber::log::debug, std::ostringstream() << "[DBG] " << loc.file_name() << '(' << loc.line() << ',' << loc.column() << ") " << loc.function_name() << ": \t" << x); } while (false)
+#define LOG_INFO(x)     do { const auto loc = std::source_location::current(); amber::log::write(amber::log::info,  std::ostringstream() << "[INF] " << loc.file_name() << '(' << loc.line() << ',' << loc.column() << ") " << loc.function_name() << ": \t" << x); } while (false)
+#define LOG_WARN(x)     do { const auto loc = std::source_location::current(); amber::log::write(amber::log::warn,  std::ostringstream() << "[WRN] " << loc.file_name() << '(' << loc.line() << ',' << loc.column() << ") " << loc.function_name() << ": \t" << x); } while (false)
+#define LOG_ERROR(x)    do { const auto loc = std::source_location::current(); amber::log::write(amber::log::error, std::ostringstream() << "[ERR] " << loc.file_name() << '(' << loc.line() << ',' << loc.column() << ") " << loc.function_name() << ": \t" << x); } while (false)
+#define LOG_FATAL(x)    do { const auto loc = std::source_location::current(); amber::log::write(amber::log::fatal, std::ostringstream() << "[FTL] " << loc.file_name() << '(' << loc.line() << ',' << loc.column() << ") " << loc.function_name() << ": \t" << x); } while (false)
+#define LOG_ERRNO()     errno << ' ' << '"' << strerror(errno) << '"' << ' '
 
 #endif
